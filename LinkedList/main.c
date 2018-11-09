dd077c919857371d66765f0d1e7694da3a9363b6
Mohammad Salek <salek.mohmd@gmail.com>
2018-11-09 15:04:01 +0330

    Update main.c

--- LinkedList/main.c
+++ LinkedList/main.c
@@ -2,77 +2,61 @@
 
 int main() {
 
-    //create head of the list:
-    node_t *head;
-
-    //create nodes:
+/* nodes sample1: */
     node_t *node1 = createNode(100);
     node_t *node2 = createNode(200);
     node_t *node3 = createNode(300);
     node_t *node4 = createNode(400);
     node_t *node5 = createNode(500);
-
-    //link them together:
-    head = node1;
     node1->next = node2;
     node2->next = node3;
     node3->next = node4;
     node4->next = node5;
     node5->next = NULL;
 
-    //create array of nodes with random data:
-    /*
-    srand((unsigned int) time(NULL));
-    node_t *array[1000];
-    for (int i = 0; i < 1000; ++i) {
-        array[i] = createNode(rand() % 1000 + 1000);
-    }
-    for (int i = 1; i < 1000; ++i) {
-        array[i-1]->next = array[i];
-    }
-    head = array[0];
-    */
+/* nodes sample2: */
+//    srand((unsigned int) time(NULL));
+//    node_t *array[1000];
+//    for (int i = 0; i < 1000; ++i) {
+//        array[i] = createNode(rand() % 1000 + 1000);
+//    }
+//    for (int i = 1; i < 1000; ++i) {
+//        array[i-1]->next = array[i];
+//    }
 
-    //push to list:
+/* head of the list: */
+    node_t *head;
+    head = node1;  /* link head to sample1*/
+//    head = array[0]; /* or link head to sample2*/
+
+/* functions usage: */
+
 //    push(head, createNode(2));
 
-    //pop from list:
 //    int popped = pop(head);
 
-    //insert to bottom of the list:
 //    insert(&head, createNode(987));
 
-    //swap two nodes in the list:
 //    swapNodes(&head, node2, node3);
 
-    //add after a node:
 //    addNodeAfter(node2, createNode(900));
 
-    //add before a node:
 //    addNodeBefore(&head, node3, createNode(842));
 
-    //remove a node by address:
 //    removeNode(&head, node3);
 
-    //remove a node by data:
-//    removeNodeByData(&head, createData(200));
+//    removeNodeByData(&head, 200);
 
-    //size of list:
 //    printf("%d", sizeOfList(head));
 
-    //reverse list:
 //    reverseList(&head);
 
-    //sort list:
-//    sortByDataNum(&head);
+//    sortByData(&head);
 
-    //get pointer to a node by its index starting from head (index = 0):
 //    showNode(nodeByIndex(head, 0));
 
-    //shuffle the list:
 //    shuffle(&head);
 
-    //print the list:
     showNodesFrom(head);
 
     return 0;
